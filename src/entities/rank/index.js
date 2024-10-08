36c84693cbdd4d77f2068fb7f262098ec37e27e5
danielpedroo <danielpapereiraJV@gmail.com>
2024-10-08 10:22:18 -0300

    update loop tryAgain

--- src/entities/rank/index.js
+++ src/entities/rank/index.js
@@ -12,29 +12,43 @@ class Rank {
   }
 }
 
-console.log("RANK");
-const victory = readline.questionInt("Quantas vitorias tem? ");
-const loser = readline.questionInt("Quantas derrotas tem? ");
-
 let player = new Rank();
-const totalVictory = player.victoryAndLoser(victory, loser);
+let exit = false;
 
-if (totalVictory < 10) {
-  player = new Rank(totalVictory, "Ferro");
-} else if (totalVictory > 11 && totalVictory < 20) {
-  player = new Rank(totalVictory, "Bronze");
-} else if (totalVictory > 21 && totalVictory < 50) {
-  player = new Rank(totalVictory, "Prata");
-} else if (totalVictory > 51 && totalVictory < 80) {
-  player = new Rank(totalVictory, "Ouro");
-} else if (totalVictory > 81 && totalVictory < 90) {
-  player = new Rank(totalVictory, "Diamante");
-} else if (totalVictory > 91 && totalVictory < 100) {
-  player = new Rank(totalVictory, "Lendario");
-} else if (totalVictory >= 101) {
-  player = new Rank(totalVictory, "Imortal");
-}
+do {
+  console.log("RANK");
+  const victory = readline.questionInt("Quantas vitorias tem?: ");
+  const loser = readline.questionInt("Quantas derrotas tem?: ");
+  const totalVictory = player.victoryAndLoser(victory, loser);
+  
+  if (totalVictory < 10) {
+    player = new Rank(totalVictory, "Ferro");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  } else if (totalVictory > 11 && totalVictory < 20) {
+    player = new Rank(totalVictory, "Bronze");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  } else if (totalVictory > 21 && totalVictory < 50) {
+    player = new Rank(totalVictory, "Prata");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  } else if (totalVictory > 51 && totalVictory < 80) {
+    player = new Rank(totalVictory, "Ouro");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  } else if (totalVictory > 81 && totalVictory < 90) {
+    player = new Rank(totalVictory, "Diamante");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  } else if (totalVictory > 91 && totalVictory < 100) {
+    player = new Rank(totalVictory, "Lendario");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  } else if (totalVictory >= 101) {
+    player = new Rank(totalVictory, "Imortal");
+    console.log(`O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`);
+  }else{
+    console.log("Nivel de ranking não definido!");
+  }
+  console.log("------------------------------------------------")
+  console.log("Tentar novamente? (s/n)");
+  let tryagain = readline.question();
+  exit = tryagain === 's'? true : false;
+  console.log("------------------------------------------------")
 
-console.log(
-  `O Heroi tem um saldo de ${player.victory} esta no nivel de ${player.rank}`
-);
+} while (exit);
